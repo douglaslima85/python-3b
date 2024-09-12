26da046038b49daa94f0925270980b37b7ac7d5f
Douglas dos Reis de Lima <106695841+douglaslima85@users.noreply.github.com>
2024-09-12 10:32:55 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-print('Camisetas\n')
+import os
 
-print('1. Cadastro de camisetas')
-print('2. Lista de camisetas')
-print('3. Ativar camisetas')
-print('4. Sair da aplicação')
+def mostra_titulo():
+ print(''''
+      
+  𝒄𝒂𝒎𝒊𝒔𝒆𝒕𝒂𝒔 
+      
+ ''')
 
-opcao_escolhida = int(input('Escolha uma opção: ') )
+def mostra_escolhas():
+ print('1. Cadastro de camisetas')
+ print('2. Lista de camisetas')
+ print('3. Ativar camisetas')
+ print('4. Sair da aplicação')
+
+
+
+def escolhe_opcao():
+ opcao_escolhida = int(input('Escolha uma opção: ') )
 print('Você escolheu a opção: ' , opcao_escolhida)
+
+def finalizar_programa():
+    os.system('cle')
+    print('Finalizando programa')
+
+
 
 if opcao_escolhida == 1:
     print('Cadastrar camisetas')
@@ -16,3 +33,11 @@ elif opcao_escolhida == 3:
     print('Ativar/desativar camisetas')
 else:
     print('Saindo da aplicação')
+    
+def main():
+    mostra_titulo()
+    mostra_escolhas()
+    escolhe_opcao()
+    
+if __name__ == '__main__':
+    main()
