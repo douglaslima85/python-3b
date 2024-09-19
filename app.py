818b8ed39c07d271d66ec7d1ed49e9d4807cc6ef
Douglas dos Reis de Lima <106695841+douglaslima85@users.noreply.github.com>
2024-09-19 10:28:10 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,21 +8,25 @@ def mostra_titulo():
  ''')
 
 def mostra_escolhas():
- print('1. Cadastro de camisetas')
- print('2. Lista de camisetas')
- print('3. Ativar camisetas')
- print('4. Sair da aplicação')
+  print('1. Cadastro de camisetas')
+  print('2. Lista de camisetas')
+  print('3. Ativar camisetas')
+  print('4. Sair da aplicação')
 
 
 
 def escolhe_opcao():
- opcao_escolhida = int(input('Escolha uma opção: ') )
-print('Você escolheu a opção: ' , opcao_escolhida)
+     try:
+      opcao_escolhida = int(input('Escolha uma opção: ') )
+     print('Você escolheu a opção: ' , opcao_escolhida)
 
-def finalizar_programa():
-    os.system('cle')
-    print('Finalizando programa')
-
+     def finalizar_programa():
+      os.system('cle')
+     print('Finalizando programa')
+ def opcao_invalida():
+  print('Esse caracter não é permitido')
+input('Digite qualquer tecla')
+main()
 
 
 if opcao_escolhida == 1:
@@ -31,8 +35,10 @@ elif opcao_escolhida == 2:
     print('Lista de camisetas')
 elif opcao_escolhida == 3:
     print('Ativar/desativar camisetas')
+elif opcao_escolhida == 4:
+   finalizar_progama()
 else:
-    print('Saindo da aplicação')
+    opcao_invalida() 
     
 def main():
     mostra_titulo()
